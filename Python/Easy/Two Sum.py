30fef677293f806fc1294c523ee34cd34c43a20b
Timur <140198814+satymbaevtimur@users.noreply.github.com>
2023-11-13 13:53:53 +0600

    Create Two Sum.py

---
+++ Python/Easy/Two Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in hashMap:
+                return [i, hashMap[complement]]
+
+            hashMap[nums[i]] = i
